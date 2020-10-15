f48a68b272f9edf975017e2c6cb2b46885f800c0
q4speed <kun@fit2cloud.com>
2020-10-15 10:19:31 +0800

    fix: 修复字段获取错误

--- src/jmeter/components/others/thread-group/index.js
+++ src/jmeter/components/others/thread-group/index.js
@@ -4,13 +4,13 @@ export default class ThreadGroup extends HashTreeElement {
   constructor(options = {}) {
     super(options);
 
-    this.onSampleError = this.initStringProp(this.props, 'TestPlan.on_sample_error');
-    this.numThreads = this.initStringProp(this.props, 'TestPlan.num_threads', 1);
-    this.rampTime = this.initStringProp(this.props, 'TestPlan.ramp_time', 1);
+    this.onSampleError = this.initStringProp(this.props, 'ThreadGroup.on_sample_error');
+    this.numThreads = this.initStringProp(this.props, 'ThreadGroup.num_threads', 1);
+    this.rampTime = this.initStringProp(this.props, 'ThreadGroup.ramp_time', 1);
 
     let loopController = this.props['ThreadGroup.main_controller'];
     this.continueForever = this.initBoolProp(loopController.elements, 'ThreadGroup.continue_forever');
-    this.loops = this.initBoolProp(loopController.elements, 'ThreadGroup.loops');
+    this.loops = this.initStringProp(loopController.elements, 'ThreadGroup.loops', 1);
 
     this.sameUserOnNextIteration = this.initBoolProp(this.props, 'ThreadGroup.same_user_on_next_iteration');
     this.delayedStart = this.initBoolProp(this.props, 'ThreadGroup.delayedStart');
