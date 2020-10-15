117893418d8f46dc1efecccafac933eaab47ede5
q4speed <kun@fit2cloud.com>
2020-10-15 12:59:30 +0800

    feat: 完成UnSupportedComponent

---
+++ src/jmeter/components/others/unspported-component/src/main.vue
@@ -0,0 +1,33 @@
+<template>
+  <component-container :object="object">
+    <el-form ref="object" :model="object" size="mini">
+      <div class="un-support">
+        暂不支持，敬请期待
+      </div>
+    </el-form>
+  </component-container>
+</template>
+
+<script>
+
+import ComponentContainer from "@/components/ComponentContainer";
+import UnsupportedComponent from "@/jmeter/components/others/unspported-component";
+
+export default {
+  name: "UnsupportedComponent",
+  components: {ComponentContainer},
+  props: {
+    object: UnsupportedComponent
+  }
+}
+</script>
+
+<style scoped>
+.un-support {
+  width: 100%;
+  height: 200px;
+  margin: 50px 0;
+  font-size: 20px;
+  text-align: center;
+}
+</style>
