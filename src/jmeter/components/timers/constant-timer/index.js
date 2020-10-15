803429882015f39855d721ec459e059232469b24
q4speed <kun@fit2cloud.com>
2020-10-15 12:58:42 +0800

    feat: 完成ConstantTimer

--- src/jmeter/components/timers/constant-timer/index.js
+++ src/jmeter/components/timers/constant-timer/index.js
@@ -3,5 +3,6 @@ import HashTreeElement from "@/jmeter/hashtree";
 export default class ConstantTimer extends HashTreeElement {
   constructor(options = {}) {
     super(options);
+    this.delay = this.initStringProp(this.props, 'ConstantTimer.delay');
   }
 }
