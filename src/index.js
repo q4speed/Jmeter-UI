4ed74563ae9fed05e47fce4378704570243814de
q4speed <kun@fit2cloud.com>
2020-11-13 14:44:52 +0800

    feat: 允许外部自定义组件

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ const install = function (Vue, opts = {}) {
   locale.use(opts.locale);
   locale.i18n(opts.i18n);
 
-  components.keys().map(key => {
+  components.keys().forEach(key => {
     let component = components(key).default;
+    // 允许外部自定义组件
+    if (opts.components) {
+      let oc = opts.components.find(c => {
+        return c.name === component.name;
+      })
+      component = oc ? oc : component;
+    }
     Vue.component(component.name, component);
   })
 };
