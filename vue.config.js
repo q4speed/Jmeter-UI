fcadc6b9889b973338fc8d110f65811c21ddfdcf
q4speed <kun@fit2cloud.com>
2020-11-20 18:21:56 +0800

    测试

--- vue.config.js
+++ vue.config.js
@@ -12,42 +12,11 @@ module.exports = {
     devtool: 'source-map',
     resolve: {
       alias: {
-        '@': resolve('src')
+        '@': resolve('./src')
       }
     },
-    entry: './src/index.js',
     output: {
-      filename: 'jmeter-ui.min.js',
       library: 'JMeterUI',
-      libraryTarget: 'umd',
-      umdNamedDefine: true
-    },
-    module: { // loader
-      rules: [{
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-        {
-          test: /\.(png|jpg|gif|svg)$/,
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]?[hash]'
-          }
-        }
-      ]
-    },
-    externals: {
-      vue: {
-        root: 'Vue',
-        commonjs: 'vue',
-        commonjs2: 'vue',
-        amd: 'vue'
-      }
-    },
+    }
   }
 };
